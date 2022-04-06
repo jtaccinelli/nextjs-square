41a72ffe8a336a2bbd8ba629cf5e688f6f123096
Jordan Accinelli <jordan.accinelli@threebyone.com.au>
2022-04-06 14:53:30 +1000

    Added basic elements to Square template, and created effective helper methods

---
+++ store/catalog/reducer.js
@@ -0,0 +1,12 @@
+export const initialState = {
+  lastUpdated: Date.now(),
+  catalog: [],
+};
+
+const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case UPDATE
+    }
+};
+
+export default reducer;
